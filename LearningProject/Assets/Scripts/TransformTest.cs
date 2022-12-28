d3e79db7b136ab1c0bf71f8d1a74a83f8a2ba58e
GabLunaDev <gabriellunacv@gmail.com>
2022-12-27 22:15:01 -0300

    Using Transform component

---
+++ LearningProject/Assets/Scripts/TransformTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TransformTest : MonoBehaviour {
+    
+    Vector3 pos;
+    
+    private void Start() {
+        
+        transform.position = pos;
+
+    }
+
+    private void Update() {
+        
+    }
+}
