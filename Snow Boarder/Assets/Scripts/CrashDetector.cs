37ddc0b4ebeb925e49076a50baf81b087f3c8137
GabLunaDev <gabriellunacv@gmail.com>
2023-10-18 10:01:45 -0300

    Adding particle system

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -3,23 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect;
     Collider2D headCollider;
-
 
     void Start()
     {
         headCollider = GetComponent<CircleCollider2D>();
+        PauseEffect();
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Floor") && headCollider.IsTouching(other)){
             SurfaceEffector2D surfaceEffector = other.GetComponent<SurfaceEffector2D>();
             surfaceEffector.speed = 1.0f;
+            crashEffect.Play();
+            Invoke(nameof(PauseEffect), 2f);
             Invoke(nameof(RestartLevel), 2f);
         }
     }
 
     void RestartLevel(){
         SceneManager.LoadScene("Level-1");
+    }
+
+    void PauseEffect(){
+        crashEffect.Pause();
     }
 }
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -5,14 +5,17 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] GameObject cameraToHold;
+    [SerializeField] ParticleSystem finishEffect;
     CinemachineVirtualCamera vc;
 
     void Start(){
         vc = cameraToHold.GetComponent<CinemachineVirtualCamera>();
+        finishEffect.Pause();
     }
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             vc.Follow = gameObject.transform;
+            finishEffect.Play();
             Invoke(nameof(RestartLevel), 2f);
         }
     }
